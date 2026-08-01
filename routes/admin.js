31b1d3ad9e90b67fee882d058bfd81aba4d04043
Clement-les98 <mamarobamogano@gmail.com>
2026-08-01 21:50:00 +0200

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,50 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
 
 console.log("Admin routes loaded");
 
-const verifyToken =
-require("../middleware/authMiddleware");
-
-router.get("/login", (req, res) => {
-
-    res.sendFile(
-        path.join(
-            __dirname,
-            "../admin/login.html"
-        )
-    );
-
+router.get("/test", (req, res) => {
+    res.send("Admin router is working");
 });
 
-router.get(
-    "/dashboard",
-    verifyToken,
-    (req, res) => {
-
-        res.sendFile(
-            path.join(
-                __dirname,
-                "../admin/dashboard.html"
-            )
-        );
-
-    }
-);
-
-router.get(
-    "/add-job",
-    verifyToken,
-    (req, res) => {
-
-        res.sendFile(
-            path.join(
-                __dirname,
-                "../admin/add-job.html"
-            )
-        );
-
-    }
-);
 module.exports = router;
