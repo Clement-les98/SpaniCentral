d7c175c40be65a12dd509f6548ae40c2ea950092
Clement-les98 <mamarobamogano@gmail.com>
2026-08-01 22:09:35 +0200

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,16 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+const fs = require("fs");
+
 router.get("/login", (req, res) => {
 
-    const filePath = path.join(
-        __dirname,
-        "../admin/login.html"
-    );
+    const filePath = path.join(__dirname, "../admin/login.html");
 
-    console.log(filePath);
-
-    res.sendFile(filePath);
+    res.json({
+        filePath,
+        exists: fs.existsSync(filePath)
+    });
 
 });
 module.exports = router;
